221b862eb180a5d881d9afaba9b4f3d1b564c7e6
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2020-02-15 10:07:17 +0100

    Doe hetzelfde voor de Message class, onze app moet immers weten dat de unread property is aangepast.

--- src/js/Message.js
+++ src/js/Message.js
@@ -1,3 +1,5 @@
+import {decorate, observable, action} from 'mobx';
+
 class Message {
 
   constructor({content, user, unread = false}) {
@@ -6,5 +8,10 @@ class Message {
     this.unread = unread;
   }
 }
+
+decorate(Message, {
+  unread: observable,
+  setUnread: action
+});
 
 export default Message;
